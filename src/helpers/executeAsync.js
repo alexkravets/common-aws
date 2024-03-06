830afe742a2fde535536df7016d9060cbff47227
Oleksandr Kravets <a@kra.vc>
2024-03-06 11:13:51 +0200

    Expose execute async helper

--- src/helpers/executeAsync.js
+++ src/helpers/executeAsync.js
@@ -2,11 +2,11 @@
 
 const getService = require('./getService')
 
-const execute = async (serviceName, operationId, parameters = {}) => {
+const executeAsync = async (serviceName, operationId, parameters = {}) => {
   const service = getService(serviceName)
   const result = await service.executeAsync(operationId, parameters)
 
   return result
 }
 
-module.exports = execute
+module.exports = executeAsync
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@
 const execute       = require('./helpers/execute')
 const AwsService    = require('./AwsService')
 const MockService   = require('./MockService')
+const executeAsync  = require('./helpers/executeAsync')
 const LambdaService = require('./LambdaService')
 
 module.exports = {
   execute,
   AwsService,
   MockService,
-  LambdaService
+  executeAsync,
+  LambdaService,
 }
